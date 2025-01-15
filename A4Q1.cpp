5667a61bc269ee05723d94656c7c0550c5644747
Mohsin Rasul <mohsinrasul80@gmail.com>
2025-01-16 00:54:27 +0500

    Create A4Q1.cpp

---
+++ A4Q1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Student {
+    string name;
+    string rollNumber;
+    float marks;
+};
+
+int main() {
+    Student students[2];
+
+    for (int i = 0; i < 2; ++i) {
+        cout << "Enter details for student " << i + 1 << ":" << endl;
+        cout << "Name: ";
+        cin.ignore();  
+        getline(cin, students[i].name); 
+        cout << "Roll Number: ";
+        cin >> students[i].rollNumber;  
+        cout << "Marks: ";
+        cin >> students[i].marks; 
+        cout << endl;
+    }
+
+    cout << "Student Details:" << endl;
+    for (int i = 0; i < 2; ++i) {
+        cout << "Student " << i + 1 << ":" << endl;
+        cout << "Name: " << students[i].name << endl;
+        cout << "Roll Number: " << students[i].rollNumber << endl;
+        cout << "Marks: " << students[i].marks << endl;
+        cout << endl;
+    }
+
+    return 0;
+}
